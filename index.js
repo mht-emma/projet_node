d2c41b119286cef5a0df522c486c1c0d374edc2a
Mahamat Emma <mahamatemma01@gmail.com>
2025-10-18 17:00:42 +0100

    Ajout de la methode soustraction dans index.js

--- index.js
+++ index.js
@@ -6,9 +6,15 @@ function addition(a,b) {
 return a+b; 
 }
 
+function soustraction(a,b){
+return a-b;
+}
+
 if(require.main==module) {
 
 console.log("Resultat : ",addition(5,3));
+console.log("Resultat : ",soustraction(5,3));
+
 }
 
-module.exports={ addition };
+module.exports={ addition,soustraction };
