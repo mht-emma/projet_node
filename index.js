f69368b67fb44503c3efb3e890f478ebc4ee55c2
Mahamat Emma <mahamatemma01@gmail.com>
2025-10-18 18:17:55 +0100

    test bugfix

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //index.js (branche dev)
 console.log('Hello node');
 // index.js (bugfix) – rendre addition plus robuste 
+//modif depuis bugfix
 function addition(a, b) { 
 const x = Number(a), y = Number(b); 
 if (Number.isNaN(x) || Number.isNaN(y)) return 0; 
